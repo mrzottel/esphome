1fe131c2f5dcd6ba3393dc01527f3018bf0f156d
mrzottel <mrzottel@gmail.com>
2025-06-30 18:14:19 +0200

    Update __init__.py

--- esphome/components/fluval_ble_led/number/__init__.py
+++ esphome/components/fluval_ble_led/number/__init__.py
@@ -28,7 +28,7 @@ def validate_min_max(config):
 
 
 CONFIG_SCHEMA = cv.All(
-    number.number_schema(
+    number.number_schema.extend(
         {
             cv.GenerateID(): cv.declare_id(FluvalBleChannelNumber),
             cv.Required(CONF_CHANNEL): int,
